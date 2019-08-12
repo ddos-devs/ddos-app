c089d7c5cab5535fc9051140b12b825c82c140ee
Zeev <zeev@tuqqi.com>
2019-08-12 19:01:53 +0300

    fix: load the background-image after the first render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.png';
 import ListItem from "./images/listItemWhite.png";
 import config from './config';
-import {Link} from "@material-ui/core"
+import { Link } from "@material-ui/core"
 import './App.css';
 import styled from "styled-components";
+import classnames from "classnames";
 
 const LinksContainer = styled.div`
     display: flex;
@@ -14,7 +15,7 @@ const LinksContainer = styled.div`
     margin-right: 17%;
 `;
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link) `
   display: flex;
   flex-direction: column;
 `;
@@ -28,48 +29,63 @@ const RightSide = styled.div`
   flex: 50;
 `;
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1>
-                    קהילת המפתחים החרדים ושומרי התורה
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            loaded: false
+        }
+    }
+
+    componentDidMount(){
+    
+        // TODO: you can add settimeout before change the state
+        this.setState({ loadded: true })
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <header className={classnames( "App-header", (this.state.loadded && 'app-header-background' ))}>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>
+                        קהילת המפתחים החרדים ושומרי התורה
                 </h1>
-                <div className='description-container'>
-                    <RightSide>
-                        <Description>
-                            {config.description}
-                        </Description>
-                    </RightSide>
-                    <ul className="list">
-                        {config.targets.map(target =>
-                            <li key={target} className="list-item">
-                                <img src={ListItem}/>
-                                <h3>{target}</h3>
-                            </li>
-                        )}
-                    </ul>
-                </div>
+                    <div className='description-container'>
+                        <RightSide>
+                            <Description>
+                                {config.description}
+                            </Description>
+                        </RightSide>
+                        <ul className="list">
+                            {config.targets.map(target =>
+                                <li key={target} className="list-item">
+                                    <img src={ListItem} />
+                                    <h3>{target}</h3>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
 
-                <h4 className='bottom-line'>יחד מצמצמים את הפערים בחברה ומקדמים את הציבור שלנו לקדמת התעשייה!</h4>
-            </header>
+                    <h4 className='bottom-line'>יחד מצמצמים את הפערים בחברה ומקדמים את הציבור שלנו לקדמת התעשייה!</h4>
+                </header>
 
-            <p>
-                <h2>הצטרפו אלינו!</h2>
-                <LinksContainer>
-                    {config.networks.map(network => {
-                        return (
-                            <StyledLink key={network.displayName} className="network-icon" href={network.url}>
-                                <img src={network.icon}/>
-                                <span>{network.displayName}</span>
-                            </StyledLink>
-                        )
-                    })}
-                </LinksContainer>
-            </p>
-        </div>
-    );
+                <p>
+                    <h2>הצטרפו אלינו!</h2>
+                    <LinksContainer>
+                        {config.networks.map(network => {
+                            return (
+                                <StyledLink key={network.displayName} className="network-icon" href={network.url}>
+                                    <img src={network.icon} />
+                                    <span>{network.displayName}</span>
+                                </StyledLink>
+                            )
+                        })}
+                    </LinksContainer>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
